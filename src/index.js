922f13a183cb1b13e0a568fafd7cd096601da4e9
Rafa Torres <rafaelfeitozatorres@gmail.com>
2018-08-29 13:08:12 -0300

    Exclusão dos arquivos

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Master from './pages/master';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Master />, document.getElementById('root'));
-registerServiceWorker();
