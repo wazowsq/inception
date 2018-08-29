ede6f18bee8bf2e00472a096e56e0605201b4fb8
Rafa Torres <rafaelfeitozatorres@gmail.com>
2018-08-29 14:08:58 -0300

    problema com o gulp novamente devido a instalação da dependência

--- README.md
+++
@@ -1,1 +0,0 @@
-#rafaeltorr.es
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Master from './master';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Master />, document.getElementById('root'));
-registerServiceWorker();
