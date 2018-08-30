058bf2ab0fd9089ed3959573acd846876e4d7aa9
Rafa Torres <rafaelfeitozatorres@gmail.com>
2018-08-30 13:10:52 -0300

    Estilo do menu de navegação pronto

--- src/include/navbar/index.js
+++ src/include/navbar/index.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <nav>
             <h1><a href="" title="App4User">app4user</a></h1>
             <ul className="nav">
-                <li><a href="" title="Home">Home</a></li>
+                <li><a href="" title="Home" className="active">Home</a></li>
                 <li><a href="" title="About Me">About Me</a></li>
                 <li><a href="" title="Experience">Experience</a></li>
                 <li><a href="" title="Portfolio">Portfolio</a></li>
