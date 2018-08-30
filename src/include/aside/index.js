62f98b71729efe4849c888ee27a7f5b8db32ad74
Rafa Torres <rafaelfeitozatorres@gmail.com>
2018-08-30 12:16:55 -0300

    Criação das seções obrigatórias

--- src/include/aside/index.js
+++ src/include/aside/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <aside>a
+        <aside>
+            <article>
+                <section className="home">
+                    a
+                </section>
+                <section className="about">
+                    b
+                </section>
+                <section className="experience">
+                    c
+                </section>
+                <section className="portfolio">
+                    d
+                </section>
+                <section className="contact">
+                    e
+                </section>
+                <section className="blog">
+                    f
+                </section>
+            </article>
         </aside>
     );
 }
