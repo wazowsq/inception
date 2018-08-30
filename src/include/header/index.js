fa234b4451e4ac12fa173f75a40f8a6364421bcb
Rafa Torres <rafaelfeitozatorres@gmail.com>
2018-08-30 12:25:39 -0300

    Definição da estrutura do navbar

--- src/include/header/index.js
+++ src/include/header/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Navbar from '../navbar';
 import ContactNav from '../contact';
 import SocialNav from '../social';
 
 const Header = () => {
     return (
         <header>
+            <Navbar />
             <ContactNav />
             <SocialNav />
         </header>
