be8cfae872446bacb5c5c94266499e9a97e591f6
Rafa Torres <rafaelfeitozatorres@gmail.com>
2018-08-30 11:35:50 -0300

    Alteração da estrutura do react para as navs bars

--- src/include/header/index.js
+++ src/include/header/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 
 import ContactNav from '../contact';
+import SocialNav from '../social';
 
 const Header = () => {
     return (
         <header>
             <ContactNav />
-            <ul className="social">
-                <li><a href="" title="Twitter"><i className="fab fa-twitter"></i></a></li>
-                <li><a href="" title="Facebook"><i className="fab fa-facebook-f"></i></a></li>
-                <li><a href="" title="Instagram"><i className="fab fa-instagram"></i></a></li>
-                <li><a href="" title="Linkedin"><i className="fab fa-linkedin-in"></i></a></li>
-                <li><a href="" title="Github"><i className="fab fa-github"></i></a></li>
-                <li><a href="" title="Behance"><i className="fab fa-behance"></i></a></li>
-                <li><a href="" title="Youtube"><i className="fab fa-youtube"></i></a></li>
-            </ul>
+            <SocialNav />
         </header>
     );
 }
---
+++ src/include/social/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SocialNav = () => {
+    return (
+        <ul className="social">
+            <li><a href="" title="Twitter"><i className="fab fa-twitter"></i></a></li>
+            <li><a href="" title="Facebook"><i className="fab fa-facebook-f"></i></a></li>
+            <li><a href="" title="Instagram"><i className="fab fa-instagram"></i></a></li>
+            <li><a href="" title="Linkedin"><i className="fab fa-linkedin-in"></i></a></li>
+            <li><a href="" title="Github"><i className="fab fa-github"></i></a></li>
+            <li><a href="" title="Behance"><i className="fab fa-behance"></i></a></li>
+            <li><a href="" title="Youtube"><i className="fab fa-youtube"></i></a></li>
+        </ul>
+    );
+}
+
+export default SocialNav;
